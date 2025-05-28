5b1bc4c4198a70a8629e9dafd8fe5f906ff55225
Mfreitas0 <mfreitasc0@gmail.com>
2025-05-28 19:48:02 -0300

    Atualiza o estilo do perfil.

--- fast_madr/static/js/create_book.js
+++ fast_madr/static/js/create_book.js
@@ -1,65 +1,102 @@
-const modal = document.getElementById("book-modal");
-const openModal = document.getElementById("open-modal");
-const closeModal = document.querySelector(".close");
+document.addEventListener('DOMContentLoaded', function() {
 
-openModal.addEventListener("click", () => {
-  modal.style.display = "block";
-})
+  const bookModal = document.getElementById("book-modal");
+  const openModalBtn = document.getElementById('open-modal');
+  const closeModalBtn = document.querySelector(".close");
 
-closeModal.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+  if (openModalBtn) {
+    openModalBtn.addEventListener("click", () => {
+      bookModal.style.display = "flex";
 
-// Fecha modal ao clicar fora
-window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
-});
-
-
-document.getElementById("book-form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const year = parseInt(document.getElementById("year").value, 10);
-  const access_token = localStorage.getItem("access_token");
-  const fileInput = document.getElementById("file_book");
-
-  const file = fileInput.files[0];
-
-  if (!file) {
-    alert("Selecione um Arquivo.");
-    return;
+      setTimeout(() => {
+        bookModal.classList.add('show');
+      }, 10);
+    });
+  } else {
+    console.warn('Elemento (Novo Livro) não encontrado.')
   }
 
-  console.log(file.name)
+  if (closeModalBtn) {
+    closeModalBtn.addEventListener("click", () => {
+      bookModal.style.display = "none";
 
-  const formData = new FormData();
-  formData.append("file", file);
-  formData.append("titulo", title);
-  formData.append("ano", year);
-  formData.append("author", author);
+      setTimeout(() => {
+        bookModal.style.display = 'none';
+      }, 300);
+    });
+  } else {
+    console.warn('Elemento (Fechar) não encontrado.')
+  }
 
-  try {
-
-    const response = await fetch("/create_book", {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${access_token}`,
-        // "Content-Type": "application/json"
-      },
-      body: formData,
-    })
-
-    if (!response.ok) {
-      throw new Error(`Erro na requisição: ${response.status}`)
+  // Fecha modal ao clicar fora
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      bookModal.classList.remove('show');
+      setTimeout(() => {
+        bookModal.style.display = "none";
+      }, 300)
     }
-    alert("success.")
-    modal.style.display = "none";
+  });
 
-  } catch (error) {
-    alert(error)
+
+  const bookForm = document.getElementById('book-form');
+
+  if (bookForm) {
+
+    bookForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const title = document.getElementById("title").value;
+      const author = document.getElementById("author").value;
+      const year = parseInt(document.getElementById("year").value, 10);
+      const access_token = localStorage.getItem("access_token");
+      const fileInput = document.getElementById("file_book");
+
+      const file = fileInput.files[0];
+
+      if (!file) {
+        alert("Selecione um Arquivo.");
+        return;
+      }
+
+      console.log(file.name)
+
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("titulo", title);
+      formData.append("ano", year);
+      formData.append("author", author);
+
+      try {
+
+        const response = await fetch("/create_book", {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer ${access_token}`,
+            // "Content-Type": "application/json"
+          },
+          body: formData,
+        })
+
+        if (!response.ok) {
+          throw new Error(`Erro na requisição: ${response.status}`)
+        }
+        alert("success.")
+        bookModal.style.display = "none";
+
+        bookModal.classList.remove('show');
+        setTimeout(() => {
+          bookModal.style.display = none;
+        }, 300)
+
+        this.location.reload()
+        bookForm.reset();
+
+      } catch (error) {
+        alert(error)
+      }
+    });
+  } else {
+    console.warn('Elemento (book-form) não encontrado.');
   }
-});
+}); 
--- fast_madr/static/js/profile_book_list.js
+++ fast_madr/static/js/profile_book_list.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", async function() {
   const access_token = localStorage.getItem("access_token");
-  console.log(access_token);
+
+  // --- Funções de Fetch e Renderização de Livros ---
   async function fetchUserBooks() {
     try {
       const response = await fetch("/user/info_user", {
@@ -9,12 +10,10 @@ document.addEventListener("DOMContentLoaded", async function() {
           Authorization: `Bearer ${access_token}`,
         },
       });
-
       if (!response.ok) {
         console.log("Erro na requisição:", response.status);
         return [];
       }
-
       const data = await response.json();
       console.log("livros recebidos: ", data.created_books);
       return data.created_books || [];
@@ -24,44 +23,66 @@ document.addEventListener("DOMContentLoaded", async function() {
     }
   }
 
-
   function renderUserBooks(books) {
-    const modal_edit_book = document.getElementById('inf-modal');
+    const modalInf = document.getElementById('inf-modal'); // Referência ao modal de informações
     const container = document.getElementById("created_books");
-    container.innerHTML = "";
+    container.innerHTML = ""; // Limpa antes de adicionar
+
     books.forEach((book) => {
       const bookElement = document.createElement("div");
       bookElement.classList.add("book");
-
       bookElement.setAttribute('data-id', book.id);
-      bookElement.addEventListener('click', () => {
 
-        modal_edit_book.style.display = 'block';
+      // Adiciona o evento de clique a cada livro criado dinamicamente
+      bookElement.addEventListener('click', () => {
+        console.log(`Livro com ID ${bookElement.dataset.id} clicado!`);
+
+        document.getElementById('modal-title').textContent = book.titulo;
+        document.getElementById('modal-author').textContent = `Autor: ${book.author}`;
+        document.getElementById('modal-year').textContent = `Ano: ${book.ano}`;
+        document.getElementById('modal-sinopse').textContent = book.sinopse;
+        // document.getElementById('modal-cover').src = book.cover_url; 
+        // document.getElementById('link').href = book.download_url;
+
+        modalInf.style.display = 'flex';
+        setTimeout(() => {
+          modalInf.classList.add('show');
+        }, 10);
       });
 
-
       bookElement.innerHTML = `
-      <h2>${book.titulo}</h2>
-      <p>Autor: ${book.author}</p>
-      <p>Ano: ${book.ano}</p>
-      `;
+                <h2>${book.titulo}</h2>
+                <p>Autor: ${book.author}</p>
+                <p>Ano: ${book.ano}</p>
+            `;
       container.appendChild(bookElement);
+    });
+
+    const closeEditBookBtn = document.querySelector('.close-edit-book');
+
+    if (closeEditBookBtn) {
+      closeEditBookBtn.addEventListener('click', () => {
+        modalInf.classList.remove('show');
+        setTimeout(() => {
+          modalInf.style.display = 'none'; // Fecha o modal
+        }, 300)
+      });
+    } else {
+      console.warn("Botão '.close-edit-book' para fechar o modal não encontrado.");
+    }
+
+    // Lógica para fechar o modal clicando fora dele (no overlay)
+    window.addEventListener('click', (event) => {
+      if (event.target === modalInf) { // Verifica se o clique foi no próprio overlay do modal
+        modalInf.classList.remove('show');
+        setTimeout(() => {
+          modalInf.style.display = 'none';
+        });
+      }
     });
   }
 
+  // --- Chamada inicial para buscar e renderizar os livros ---
   const my_books = await fetchUserBooks();
   renderUserBooks(my_books);
-
-
-});
-
-
-
-// // fechar modal do livro criado
-const close_edit_book = document.querySelector('.close-edit-book');
-const modal_edit_book = document.getElementById('inf-modal');
-
-close_edit_book.addEventListener('click', () => {
-
-  modal_edit_book.style.display = "none";
 });
