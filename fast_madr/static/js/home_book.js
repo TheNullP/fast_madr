0b45ce279cce3f9e8cfe677a45eba01000541bf6
Mfreitas0 <mfreitasc0@gmail.com>
2025-05-30 18:39:00 -0300

    Conserta o problema de não indentificar o clique no livro.
    Foi mudado os IDs e adicionado o 'classList' add/remove para ajudar não
    interação mais suave.

--- fast_madr/static/js/home_book.js
+++ fast_madr/static/js/home_book.js
@@ -1,51 +1,68 @@
-const modal = document.getElementById("inf-modal");
-const close = document.querySelector(".close");
-const modal_container = document.querySelector(".modal-content");
+document.addEventListener("DOMContentLoaded", function() {
 
-document.getElementById("books-container").addEventListener("click", async (e) => {
-  const book = e.target.closest(".book");
-  if (book) {
-    modal.style.display = "block";
+  const modal = document.getElementById("inf-modal");
+  const closeBtn = document.querySelector(".close");
+  const modal_container = document.querySelector(".modal-content");
 
-    const id_book = book.dataset.id;
+  const modalDownloadLink = document.getElementById("link");
+  const modalTitle = document.getElementById('modal-title');
+  const modalAuthor = document.getElementById('modal-author');
+  const modalYear = document.getElementById('modal-year');
+  const modalSinopse = document.getElementById('modal-sinopse');
 
-    try {
-      const response = await fetch(`/page_book?id_book=${id_book}`);
+  document.getElementById("books-container").addEventListener("click", async (e) => {
+    const bookElement = e.target.closest(".book");
 
-      if (!response.ok) {
-        console.error('Erro ao buscar livro: ', response.status);
-        return;
+    if (bookElement) {
+
+      const id_book = bookElement.dataset.id;
+
+      try {
+        const response = await fetch(`/page_book?id_book=${id_book}`);
+
+        if (!response.ok) {
+          console.error('Erro ao buscar livro: ', response.status);
+          return;
+        }
+
+        const inf_modal_data = await response.json();
+
+
+        modalDownloadLink.href = inf_modal_data.url;
+        modalTitle.innerText = inf_modal_data.titulo;
+        modalAuthor.textContent = `Author: ${inf_modal_data.author}`;
+        modalYear.innerText = `Ano: ${inf_modal_data.ano}`;
+
+        modal.style.display = "flex";
+        setTimeout(() => {
+          modal.classList.add('show')
+        }, 10);
+
+      } catch (error) {
+        console.error('Erro na requisição: ', error);
       }
-
-      const inf_modal = await response.json();
-
-
-      const url = document.getElementById("link");
-      const title = document.getElementById('title');
-      const author = document.getElementById('author');
-      const year = document.getElementById('year');
-      const sinopse = document.getElementById('sinopse');
-
-
-
-      url.href = inf_modal.url;
-      title.innerText = inf_modal.titulo;
-      author.innerText = inf_modal.author;
-      year.innerText = inf_modal.ano;
-
-
-    } catch (error) {
-      console.error('Erro na requisição: ', error);
     }
-  }
-});
+  });
 
-close.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+  if (closeBtn) {
+    closeBtn.addEventListener("click", () => {
+      modal.classList.remove('show');
+      setTimeout(() => {
+        modal.style.display = "none";
+      }, 300);
+    });
+  } else {
+    console.warn('Botao close do modal não encotrado.');
 
-window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
   }
-})
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.classList.remove('show');
+      setTimeout(() => {
+        modal.style.display = "none";
+      }, 300);
+    }
+  });
+
+});
